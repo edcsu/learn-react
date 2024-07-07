4310db79bc68f020a6de4ab14df6782b73fcc9ca
Ssewannonda Keith Edwin <skeith696@gmail.com>
2024-07-07 13:53:27 +0300

    feat: add core concept component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import reactImg from  "./assets/react-core-concepts.png"
+import componentsImg from "./assets/components.png"
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
+}
+
+function CoreConcept(props) {
+  return (
+      <li>
+        <img src={props.img} alt={props.title} />
+        <h3>{props.title}</h3>
+        <p>{props.description}</p>
+      </li>
+    )
 }
 
 function Header() {
@@ -25,7 +36,13 @@ function App() {
     <div>
       <Header></Header>
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept title="Components" description= "The Core UI building bloc" img={componentsImg}/>
+
+          </ul>
+        </section>
       </main>
     </div>
   );
