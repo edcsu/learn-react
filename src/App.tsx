59a5651faf110045714343a32c2e8e4aaefda346
Ssewannonda Keith Edwin <skeith696@gmail.com>
2024-08-29 13:31:43 +0300

    refactor: pass custom arguments to functions

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import TabButton from "./components/TabButton.tsx";
 import { CORE_CONCEPTS } from "./data.ts";
 
 function App() {
-  function handleSelect() {
-    console.log('Tab clicked')
+  function handleSelect(selectedButton: string) {
+    console.log(selectedButton + ' tab clicked')
   }
 
   return (
@@ -25,10 +25,10 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={handleSelect}>Components</TabButton>
-            <TabButton onSelect={handleSelect}>JSX</TabButton>
-            <TabButton onSelect={handleSelect}>Props</TabButton>
-            <TabButton onSelect={handleSelect}>State</TabButton>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
 
         </section>
