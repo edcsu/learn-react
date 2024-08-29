d51acb55849ebd2bdae55ee83cd13c02b5c4f86f
Ssewannonda Keith Edwin <skeith696@gmail.com>
2024-08-29 13:15:20 +0300

    feat: add event listener

--- src/components/TabButton.tsx
+++ src/components/TabButton.tsx
@@ -1,8 +1,12 @@
 
 export default function TabButton(props: any) {
+    function handleClick() {
+        console.log('Tab clicked')
+    }
+
     return (
         <li>
-            <button>
+            <button onClick={handleClick}>
             {props.children}
             </button>
         </li>
