273d1dbff13b8f7935d32bdac6d51e74ab1097b5
Ssewannonda Keith Edwin <skeith696@gmail.com>
2024-08-29 12:06:32 +0300

    style: show react core concepts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import reactImg from  "./assets/react-core-concepts.png"
 import componentsImg from "./assets/components.png"
+import { CORE_CONCEPTS } from "./data.js";
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -39,7 +40,10 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept title="Components" description= "The Core UI building bloc" img={componentsImg}/>
+            <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} img={CORE_CONCEPTS[0].image}/>
+            <CoreConcept title={CORE_CONCEPTS[1].title} description={CORE_CONCEPTS[1].description} img={CORE_CONCEPTS[1].image}/>
+            <CoreConcept title={CORE_CONCEPTS[2].title} description={CORE_CONCEPTS[2].description} img={CORE_CONCEPTS[2].image}/>
+            <CoreConcept title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description} img={CORE_CONCEPTS[3].image}/>
 
           </ul>
         </section>
