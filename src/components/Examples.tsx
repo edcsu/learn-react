29432d90c261af3619480ab2fd7dfa9887915fdd
Ssewannonda Keith Edwin <skeith696@gmail.com>
2024-08-30 16:34:12 +0300

    chore: remove duplicate menu element

--- src/components/Examples.tsx
+++ src/components/Examples.tsx
@@ -27,14 +27,14 @@ function Examples() {
     
     return (
         <Section title={'Examples'} id="examples"> 
-        <Tabs buttons={<>
-                <menu>
+        <Tabs buttons={
+            <>
                 <TabButton onClick={() => handleSelect('components')} isSelected={selectedTopic === 'components'}>Components</TabButton>
                 <TabButton onClick={() => handleSelect('jsx')} isSelected={selectedTopic === 'jsx'}>JSX</TabButton>
                 <TabButton onClick={() => handleSelect('props')} isSelected={selectedTopic === 'props'}>Props</TabButton>
                 <TabButton onClick={() => handleSelect('state')} isSelected={selectedTopic === 'state'}>State</TabButton>
-            </menu>
-            </>}>
+            </>
+        }>
             {tabContent}
         </Tabs>  
         </Section>
