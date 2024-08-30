741cc15d317909c03d23212a77afee5a6552c6a7
Ssewannonda Keith Edwin <skeith696@gmail.com>
2024-08-30 13:59:53 +0300

    refactor: create components

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,16 @@
-import { useState } from 'react';
 
 import Header from "./components/Header.tsx";
-import CoreConcept from "./components/CoreConcept.tsx";
-import TabButton from "./components/TabButton.tsx";
-import { CORE_CONCEPTS, EXAMPLES } from "./data.ts";
+import CoreConcepts from './components/CoreConcepts.tsx';
+import Examples from "./components/Examples.tsx";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState()
-  function handleSelect(selectedButton: string) {
-    setSelectedTopic(selectedButton)
-  }
-
-  let tabContent = <p>Please select a topic</p>;
-
-  if (selectedTopic) {
-    tabContent = <div id='tab-content'>
-      <h3>{EXAMPLES[selectedTopic].title}</h3>
-      <p>{EXAMPLES[selectedTopic].description}</p>
-      <pre>
-        <code>
-        {EXAMPLES[selectedTopic].code}
-        </code>
-      </pre>
-    </div>
-  }
-
+  
   return (
     <>
       <Header></Header>
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((concept, index) => <CoreConcept key={index} {...concept} />)}
-          </ul>
-        </section>
-        <section id="examples"> 
-          <h2>Examples</h2>
-          <menu>
-            <TabButton onSelect={() => handleSelect('components')} isSelected={selectedTopic === 'components'}>Components</TabButton>
-            <TabButton onSelect={() => handleSelect('jsx')} isSelected={selectedTopic === 'jsx'}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect('props')} isSelected={selectedTopic === 'props'}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect('state')} isSelected={selectedTopic === 'state'}>State</TabButton>
-          </menu>
-          {tabContent}
-        </section>
+        <CoreConcepts />
+        <Examples />
       </main>
     </>
   );
